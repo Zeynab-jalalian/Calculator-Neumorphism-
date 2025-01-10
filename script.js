ce8ec9d9a88116e307b0dfe8f212c2b24875e65b
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-10 21:53:35 +0330

    js and comment

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let display=document.querySelector('#display');
-let buttons=document.querySelectorAll('.btn');
+let display=document.querySelector('#display'); //دسترسی به قسمت نمایشگر 
+let buttons=document.querySelectorAll('.btn');  //دسترسی به تک تک دکمه ها
 
-buttons.forEach(function(btn){
-  btn.addEventListener('click',function(e){
-    if(display.innerHTML==="0"){
+
+buttons.forEach(function(e){ // انتخاب تک تک دکمه ها
+    e.addEventListener('click',function(e){ // عملیات کلیک روی دکمه ها
+      if(display.innerHTML==='00'){
         display.innerHTML=e.target.innerText;
-    }else{
-        display.innerHTML+=e.target.innerText
-    }
-  })
-
- 
-  
+        // اگر 0 بود باید حذف شود و مقدار جدید جایگزین شود
+      }else{
+        display.innerHTML+=e.target.innerHTML;
+        //در غیر این صورت مقدار جدی اضافه شود
+      }
+    })
 })
