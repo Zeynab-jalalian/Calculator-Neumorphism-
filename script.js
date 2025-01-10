5b5599e76b9f659baa5aaa2fe7315fc9dd0b814c
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-10 21:48:56 +0330

    access to display and buttons js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 let display=document.querySelector('#display');
 let buttons=document.querySelectorAll('.btn');
+
+buttons.forEach(function(btn){
+  btn.addEventListener('click',function(e){
+    if(display.innerHTML==="0"){
+        display.innerHTML=e.target.innerText;
+    }else{
+        display.innerHTML+=e.target.innerText
+    }
+  })
+
+ 
+  
+})
