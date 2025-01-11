5d7655653fe355f8a24614bee5bfbd00c50adc5e
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-11 13:23:13 +0330

    AC

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let display=document.querySelector('#display'); //دسترسی به قسمت نمایشگر 
 let buttons=document.querySelectorAll('.btn');  //دسترسی به تک تک دکمه ها
-
+let AC=document.querySelector('.clear');
 
 buttons.forEach(function(e){ // انتخاب تک تک دکمه ها
     e.addEventListener('click',function(e){ // عملیات کلیک روی دکمه ها
-      if(display.innerHTML==='00'){
+      if(display.innerHTML==='0'){
         display.innerHTML=e.target.innerText;
         // اگر 0 بود باید حذف شود و مقدار جدید جایگزین شود
       }else{
         display.innerHTML+=e.target.innerHTML;
-        //در غیر این صورت مقدار جدی اضافه شود
+        //در غیر این صورت مقدار جدید کنارش  اضافه شود
       }
+    })
+    AC.addEventListener('click',function(){
+      display.innerHTML='0';
     })
 })
