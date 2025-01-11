ada41fa201fe41d93d3510ce8db7a634eb452094
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-11 14:47:26 +0330

    del

--- script.js
+++ script.js
@@ -11,17 +11,26 @@ let operator=document.querySelectorAll('.operator');
 */
 buttons.forEach(function(btn){
   btn.addEventListener('click',function(e){
+    // تعریف متغیر و مشخض کردن دکمه های دارای دیتا اکشن
+    const action=e.target.dataset.action;
+    if(!action){ //اگر عدد بود(دیتا اکشن نداشت)
     if(display.innerHTML=="0"){
       display.innerHTML=e.target.innerText;
     }else{
       display.innerHTML+=e.target.innerText;
     }
+  }
   })
 })
 // دکمه ی حذف کامل
 AC.addEventListener('click',function(){
   display.innerHTML='0';
 })
+// دکمه ی  حذف یکی یکی
 DEL.addEventListener('click',function(){
- 
+ if(display.innerHTML.length > 1){
+  display.innerHTML=display.innerHTML.slice(0,-1);
+ }else{
+  display.innerHTML="0";
+ }
 })
