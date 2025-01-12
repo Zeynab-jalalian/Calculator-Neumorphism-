8119a6035f0eb3fcce58ace76c31fd4bb7fba1cf
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-12 19:27:02 +0330

    const action

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let buttons=document.querySelectorAll('.btn');
 let AC=document.querySelector('.clear');
 let DEL=document.querySelector('.delete');
 let equal=document.querySelector('.equal');
+// حلقه برای تمام دکمه ها 
+buttons.forEach(function(button){
+  button.addEventListener('click',function(e){
+    //انتخاب تمام دکمه ها و عملیات کلیک
+    const action=e.target.dataset.action; //دکمه هایی که دیتا اکشن دارند
+    // عملگرهای ریاضی - دکمه های حذف - مساوی
+    
+  })
+})
