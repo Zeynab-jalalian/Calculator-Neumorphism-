6dfdc53b8cc395daf375fe79fb96b2f97b6343da
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-12 19:23:03 +0330

    access by document

---
+++ script.js
@@ -0,0 +1,6 @@
+// دسترسی به نمایشگر و دکمه ها
+let display=document.querySelector('#display');
+let buttons=document.querySelectorAll('.btn');
+let AC=document.querySelector('.clear');
+let DEL=document.querySelector('.delete');
+let equal=document.querySelector('.equal');
